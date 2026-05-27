dc685a0f995b89996e7a7f97b69ea064a4ff647a
sadiqhawaldarr <sadiqhawaldar00@gmail.com>
2026-05-27 12:49:51 +0530

    Update query section copy

--- src/components/QuerySection.js
+++ src/components/QuerySection.js
@@ -9,11 +9,13 @@ const QuerySection = memo(function QuerySection() {
     <section className="query-section" aria-labelledby="query-title">
       <div className="query-section__head">
         <span className="query-section__eyebrow">Help Desk</span>
-        <h2 className="query-section__title" id="query-title">Send Query</h2>
+        <h2 className="query-section__title" id="query-title">send Deatils & Query</h2>
       </div>
 
       <div className="query-contact">
-        <p className="query-contact__text">Share any query through Gmail.</p>
+        <p className="query-contact__text">
+          If you want to add your nearby masjid or update the masjid details, share the masjid name and time through the given mail.
+        </p>
         <a className="query-contact__button" href={queryMailLink} target="_blank" rel="noreferrer">
           teamfisabilillahbijapur@gmail.com
         </a>
